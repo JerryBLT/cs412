debddc8cd795f51e59f0eb8c1857bc6d3e095541
Jerry Teixeira <jerrybt@bu.edu>
2026-03-01 16:26:14 -0500

    Implemented search & results + small error fix

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -13,6 +13,7 @@ from .views import (
     ShowFollowersDetailView,
     ShowFollowingDetailView,
     PostFeedListView,
+    SearchView,
 )
 
 urlpatterns = [
@@ -26,5 +27,6 @@ urlpatterns = [
     path("profile/<int:pk>/followers", ShowFollowersDetailView.as_view(), name="show_followers"),
     path("profile/<int:pk>/following", ShowFollowingDetailView.as_view(), name="show_following"),
     path("profile/<int:pk>/feed", PostFeedListView.as_view(), name="show_feed"),
+    path("profile/<int:pk>/search", SearchView.as_view(), name="search"),
 
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -3,6 +3,8 @@
 # Discrition: View to display all user profiles in the mini_insta app.
 
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.db.models import Q
+from django.shortcuts import render
 from .models import Post, Profile, Photo
 from .forms import CreatePostForm, UpdateProfileForm, UpdatePostForm
 from django.urls import reverse
@@ -80,6 +82,7 @@ class DeletePostView(DeleteView):
     '''View to delete a post in the mini_insta app.'''
     model = Post
     template_name = 'mini_insta/delete_post_form.html'
+    context_object_name = 'post'
 
     def get_context_data(self, **kwargs):
         '''Add the Post and its Profile to the template context.'''
@@ -100,8 +103,14 @@ class UpdatePostView(UpdateView):
     context_object_name = 'post'
 
     def get_success_url(self):
-        """Return to the profile page after deleting a post."""
-        return self.object.profile.get_absolute_url()
+        """Return to the post detail page after updating a post."""
+        return reverse('show_post', kwargs={'pk': self.object.pk})
+
+    def get_context_data(self, **kwargs):
+        """Provide the post and its profile for the update form template."""
+        context = super().get_context_data(**kwargs)
+        context['profile'] = self.object.profile
+        return context
 
 
 class ShowFollowersDetailView(DetailView):
@@ -138,4 +147,51 @@ class PostFeedListView(ListView):
         """Add the profile (feed owner) to context for the template."""
         context = super().get_context_data(**kwargs)
         context['profile'] = Profile.objects.get(pk=self.kwargs['pk'])
+        return context
+
+
+class SearchView(ListView):
+    '''Search Profiles and Posts on behalf of a Profile.'''
+
+    model = Post
+    template_name = "mini_insta/search_results.html"
+    context_object_name = "posts"
+
+    def dispatch(self, request, *args, **kwargs):
+        '''If query is absent/blank in GET, show search form; otherwise run ListView.'''
+
+        query = request.GET.get('query', '').strip()
+        if not query:
+            # When there is no search query, show the form page instead of results.
+            pk = kwargs.get('pk')
+            profile = Profile.objects.get(pk=pk)
+            return render(request, 'mini_insta/search.html', {'profile': profile})
+        
+        # Otherwise let ListView handle the request and show search_results.html.
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get_queryset(self):
+        '''Return Posts whose caption contains the query string.'''
+        query = self.request.GET.get('query', '').strip()
+        if not query:
+            return Post.objects.none()
+        return Post.objects.filter(caption__icontains=query).order_by('-timestamp')
+    
+    def get_context_data(self, **kwargs):
+        '''Add profile, query, and matching profiles to context; posts from get_queryset.'''
+        context = super().get_context_data(**kwargs)
+        # Profile we are searching
+        pk = self.kwargs['pk']
+        context['profile'] = Profile.objects.get(pk=pk)
+
+        # Search term for display and for filtering profiles
+        query = self.request.GET.get('query', '').strip()
+        context['query'] = query
+
+        # Profiles matching query in username, display_name, or bio_text
+        context['profiles'] = Profile.objects.filter(
+            Q(username__icontains=query) |
+            Q(display_name__icontains=query) |
+            Q(bio_text__icontains=query)
+        )
         return context
